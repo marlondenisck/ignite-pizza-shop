c050a4ef9942899ff61819588f1ff76d4de57e2f
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-05-29 16:15:08 -0400

    Página: Dashboard

--- src/pages/app/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-export function Dashboard() {
-  return (
-    <div>
-      <title>Dashboard | Pizza Shop</title>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard!</p>
-    </div>
-  )
-}
