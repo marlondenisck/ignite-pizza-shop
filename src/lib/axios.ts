0b1a9fe2af19b88e932a4ec25bc39fd3eb5ae032
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-06-03 15:17:55 -0400

    loading state no perfil

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,23 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+
+    return config
+  })
+}
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  },
+)
